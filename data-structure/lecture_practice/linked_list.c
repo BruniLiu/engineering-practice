76196b0f84027e6a15aaa138b62038a46fe47aac
Bruni Liu <lnubruni@gmail.com>
2026-04-03 14:16:15 +0800

    print Poly

--- data-structure/lecture_practice/linked_list.c
+++ data-structure/lecture_practice/linked_list.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
-    int coef;
-    int exp;
-    struct Node *next;
-} Node;
+int* ADD(int *A, int na, int *B, int nb) {
+    int max_len = (na > nb) ? na : nb;
+    int *C = (int*)malloc(sizeof(int) * max_len);
 
-Node* createNode(int coef, int exp){
-    Node *newNode = (Node*)malloc(sizeof(Node));
-    if (newNode == NULL){
-        printf("Memory allocation failed.\n");
-        exit(1);
+    for (int i = 0; i < max_len; i++) {
+        int a = (i < na) ? A[i] : 0;
+        int b = (i < nb) ? B[i] : 0;
+        C[i] = a + b;
     }
-    newNode -> coef = coef;
-    newNode -> exp = exp;
-    newNode -> next = NULL;
-    return newNode;
+
+    return C;
+}
+
+void printPoly(int *P, int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        if (P[i] != 0) {
+            if (i != n - 1 && P[i] > 0) printf("+");
+            if (i == 0) printf("%d", P[i]);
+            else if (i == 1) printf("%dx", P[i]);
+            else printf("%dx^%d", P[i], i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int A[] = {1, 2, 3};   // 1 + 2x + 3x^2
+    int B[] = {4, 5};      // 4 + 5x
+
+    int na = 3;
+    int nb = 2;
+
+    int *C = ADD(A, na, B, nb);
+
+    printPoly(C, (na > nb ? na : nb));
+
+    free(C);
+    return 0;
 }
