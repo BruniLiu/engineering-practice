bc8ba94d9c1b8a6fc6f3566465f10044b627d6b2
Bruni Liu <lnubruni@gmail.com>
2026-04-03 14:04:46 +0800

    polysolution

--- data-structure/lecture_practice/linked_list.c
+++ data-structure/lecture_practice/linked_list.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct Node {
+    int coef;
+    int exp;
+    struct Node *next;
+} Node;
+
+Node* createNode(int coef, int exp){
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL){
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    newNode -> coef = coef;
+    newNode -> exp = exp;
+    newNode -> next = NULL;
+    return newNode;
+}
