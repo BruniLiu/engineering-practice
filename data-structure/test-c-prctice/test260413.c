5684fdd164fef2638204b362ce785aacee5cfb5f
Bruni Liu <lnubruni@gmail.com>
2026-04-13 14:37:01 +0800

    insert again at eng class

---
+++ data-structure/test-c-prctice/test260413.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MaxSize 200
+
+void InsertList(int i, int *n, int arr[], int item){
+    if(*n > MaxSize)
+        return;
+    if(i < 0 || i > *n)
+        return;
+    for(int a = *n; a > i; a--){
+        arr[a] = arr[a-1];
+    }
+    arr[i] = item;
+    (*n)++;
+}
+
+int main(){
+    int i, item;
+    int n;
+    int arr[MaxSize];
+    printf("Please type in a number randomly\n");
+    scanf("%d", &n);
+    printf("please type in the number you want in the array");
+    for(int j = 0; j < n; j++){
+        scanf("%d", &arr[j]);
+    }
+    printf("please type into the position and the number you want");
+    scanf("%d %d", &i, &item);
+    InsertList(i, &n, arr, item);
+    for(int a = 0; a < n; a++){
+        printf("%d", arr[a]);
+    }
+
+}
