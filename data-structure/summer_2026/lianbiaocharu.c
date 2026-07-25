1a0ae92e619a131c2be7a0d6c6d38f051ac95e97
Bruni Liu <lnubruni@gmail.com>
2026-07-25 13:21:39 +0800

    sha shi xing can

---
+++ data-structure/summer_2026/lianbiaocharu.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdbool.h>
+#define Maxsize 199
+
+typedef struct list
+{
+    int data[Maxsize];
+    int length;
+}SqLsit;
+
+bool Insert(SqLsit *L, int i, int value){
+    
+}
